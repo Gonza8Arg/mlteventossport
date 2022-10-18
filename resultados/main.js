30f26126617c8d7bcbffc435c8c83b1f9e40ec83
ezeet1997 <61636477+ezeet1997@users.noreply.github.com>
2022-10-18 03:18:04 -0300

    Update main.js

--- resultados/main.js
+++ resultados/main.js
@@ -2,7 +2,12 @@
 $(document).ready(function() {
     $('#example').DataTable({
         language: {
-            url: "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
-        }
+            url: "https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json",
+            stateSave: true,
+        },
+        lengthMenu: [
+            [25, 50, 100, -1],
+            [25, 50, 100, 'Todos'],
+        ],
     });
 });
